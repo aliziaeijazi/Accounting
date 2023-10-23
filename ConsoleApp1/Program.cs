9e2f6766f8a48811a63adc43345cbb272dc2c412
AliZiaei <a.ziaei@fardaadco.ir>
2023-10-23 19:08:18 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using DataLayer;
+using DataLayer.Repositories;
+using DataLayer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customerRepository = new CustomerRepository();
+
+            Customers customer = new Customers()
+            {
+                Name="محمدرضا",
+                Family="ضیایی جزی",
+                PhoneNumber="09133282279",
+                Email="MohammadRezaZiaei@gmail.com",
+                Address="اصفهان،گز برخوار",
+                CustomerImage="NoPhoto"
+            };
+            customerRepository.InsertCustomer(customer);
+            customerRepository.save();
+        }
+    }
+}
---
+++ DataLayer/services/CustomerRepository.cs
@@ -0,0 +1,79 @@
+﻿using DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+        public Customers GetCustomerById(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State=EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            try
+            {
+                Customers customer = GetCustomerById(customerId);
+                return DeleteCustomer(customer);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void save()
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
